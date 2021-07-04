688fe06b70d6fc5557578674fea4ab8968bf44b8
Edward James Remo <edward.remo@cormanttech.com>
2021-07-04 21:17:30 +0800

    initial commit

---
+++ shared/src/commonMain/kotlin/com/aguragorn/lwwgraph/shared/Greeting.kt
@@ -0,0 +1,8 @@
+package com.aguragorn.lwwgraph.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/aguragorn/lwwgraph/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.aguragorn.lwwgraph.shared
+
+expect class Platform() {
+    val platform: String
+}
